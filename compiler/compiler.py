05d8f437e9735c3ba84c07f0ceb2b18e7527b8df
cxleb <waitcaleb@gmail.com>
2018-06-03 19:51:55 +1000

    started working on complier, lexer works

---
+++ compiler/compiler.py
@@ -0,0 +1,15 @@
+from compiler import lexer
+
+def compile_il():
+    file = open("test.il", "r")
+    file_contents = file.read()
+    file.close()
+    
+    tokens = lexer.lex(file_contents)
+
+    for tok in tokens:
+        print(tok)
+
+if __name__ == "__main__":
+    compile_il()
+    
---
+++ compiler/compiler/lexer.py
@@ -0,0 +1,25 @@
+
+
+def lex(contents):
+	characters = ["{", "}", "@", "=", "+", "-", "/", "*", "<"]
+
+	content = list(contents)
+	tokens = []
+	accum = ""
+	
+	for c in content:
+		if c == " " or c == "\t" or c == "\n":
+			if accum != "":
+				tokens.append(accum)
+				accum = ""
+		elif c in characters:
+			if accum != "":
+				tokens.append(accum)
+				accum = ""
+			tokens.append(c)
+		else:
+			accum += c
+	if accum != "":
+		tokens.append(accum)
+	
+	return tokens
